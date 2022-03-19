3cfb61a373358b0bd3f245d0232e3a2f322e3f4c
armanriazi <arman2riazi@gmail.com>
2022-03-19 13:35:54 +0000

    eleven

---
+++ borrowchecker/movable-or-not/bin/rustegg-mut-movable.rs
@@ -0,0 +1,20 @@
+fn main(){
+let mut s = String::from("hello");
+
+let r1 = &s; // no problem
+let r2 = &s; // no problem
+let r3 = &mut s; // BIG PROBLEM
+
+println!("{}, {}, and {}", r1, r2, r3);
+
+//Resolved:
+/*let mut s = String::from("hello");
+
+let r1 = &s; // no problem
+let r2 = &s; // no problem
+println!("{} and {}", r1, r2);
+// variables r1 and r2 will not be used after this point
+
+let r3 = &mut s; // no problem
+println!("{}", r3);*/
+}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,17 @@
-pub trait Summary {
-    fn summarize_author(&self) -> String;
-
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
-    }
-}
-
-
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
-}
-
-//use aggregator::{Summary, Tweet};
-
-fn returns_summarizable(switch: bool) -> impl Summary {
-    //if switch {
-       /* NewsArticle {
-            headline: String::from(
-                "Penguins win the Stanley Cup Championship!",
-            ),
-            location: String::from("Pittsburgh, PA, USA"),
-            author: String::from("Iceburgh"),
-            content: String::from(
-                "The Pittsburgh Penguins once again are the best \
-                 hockey team in the NHL.",
-            ),
-        }
-    } else {*/
-        if switch {
-             Tweet {
-            username: String::from("horse_ebooks"),
-            content: String::from(
-                "of course, as you probably already know, people",
-            ),
-            reply: false,
-            retweet: false,
-        }
-    }else{
-        unimplemented!();
-    }
-}
 fn main() {
-    let tweet = returns_summarizable(true);
-
-    println!("1 new tweet: {}", tweet.summarize());
-}
+        let reference_to_nothing = dangle();
+    //let reference_to_nothing = no_dangle();
+    }
+    fn dangle() -> &'static i32 { // dangle returns a reference to a String
+    
+        let s:&'static =10;
+    
+        s // we return a reference to the String, s
+    } // Here, s goes out of scope, and is dropped. Its memory goes away.
+      // Danger!
+    
+      fn no_dangle() -> i32 {
+        let s = 100;
+    
+        s
+    }
