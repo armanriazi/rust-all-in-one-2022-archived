be2bc5a0fba5638962dbe97bb0b415d71f8bd828
armanriazi <arman2riazi@gmail.com>
2022-05-29 22:07:40 +0000

    Newupsh

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,50 @@
 
+static N:i32 = 15;
 
-pub trait Summary {
-    fn summarize_author(&self) -> String;
 
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
+pub fn get_statx()-> String{
+    "Arman".to_string()
+}
+
+pub fn get_staty<'a>()->&'a str{
+  &"Arman"
+}
+
+pub fn get_stat()->&'static i32{
+    &N
+}
+
+pub fn stat_str()->&'static str{
+    "hello"
+}
+
+static mut M:i32 = 20;
+
+pub fn add_stat(n:i32)->i32 {
+    unsafe {
+        M += n;
+        M
     }
 }
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let x = get_statx();
+        assert_eq!(*x, "Arman".to_string());
 
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
+        let y = get_staty();
+        assert_eq!(y, "Arman");
 
-impl Summary for Tweet {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
+        let n = get_stat();
+        assert_eq!(*n, 15);
+
+        let s = stat_str();
+        assert_eq!(s,"hello");
+
+        let m = add_stat(5);
+        assert_eq!(m,25);
     }
-    
-}
-
-impl Summary for NewsArticle {
-   
-
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.headline)
-    }
-}
-
-//use aggregator::{Summary, Tweet};
-
-//**fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 { */
-/* 
-fn some_function<T, U>(t: &T, u: &U) -> i32
-    where T: Display + Clone,
-          U: Clone + Debug
-{
-*/
-pub fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
-//pub fn notify(item1: &impl Summary, item2: &impl Summary) {
-    
-fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
-        reply: false,
-        retweet: false,
-    };
-
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
-        ),
-    };
-
-    notify(&tweet);
-    println!("New article available! {}", article.summarize());
 }
