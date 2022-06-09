2fe9041622b7add1c148e059e6a23c45658185f5
armanriazi <arman2riazi@gmail.com>
2022-06-09 22:51:49 +0000

    advanecd trait

---
+++ trait/associateType/rust-doc-associatetype1.rs
@@ -0,0 +1,31 @@
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // --snip--
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+/* With generic type substitute associated type
+
+pub trait Iterator<T> {
+    fn next(&mut self) -> Option<T>;
+}
+
+*/
